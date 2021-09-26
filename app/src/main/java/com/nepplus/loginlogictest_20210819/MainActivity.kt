a3cea65a657621edbeb1fe158abe11019b283fe3
김현지 <guswlhjk@naver.com>
2021-09-26 17:10:04 +0900

    응용문제 답안

--- app/src/main/java/com/nepplus/loginlogictest_20210819/MainActivity.kt
+++ app/src/main/java/com/nepplus/loginlogictest_20210819/MainActivity.kt
@@ -2,10 +2,33 @@ package com.nepplus.loginlogictest_20210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        loginBtn.setOnClickListener {
+
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+//            2. 아이디와 비번 검사.
+
+            if (inputId == "admin" && inputPw == "qwer") {
+//                로그인 성공
+                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+//                로그인 실패. (위의 경우가 아니면 모두 실패)
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
     }
 }
